ee1ada0348365654551fe1ad2efb8e59b3d40ba0
Kevin <kevin@Kevins-MBP.fios-router.home>
2018-08-22 21:41:05 -0400

    update tests

--- src/tests/parser.test.ts
+++ src/tests/parser.test.ts
@@ -2,112 +2,112 @@ import { Parser } from '../lib/public_api';
 
 const tabNewline = /\n|\t/g;
 
-test('Parses string key as string', () => {
+test('Parses string key as string', async () => {
     const parser = new Parser('{"test": "a"}', "test");
 
-    expect(parser.dump().replace(tabNewline, '')).toBe("interface test {test: string;}");
+    expect((await parser.dump()).replace(tabNewline, '')).toBe("export interface test {test: string;}");
 });
 
-test('Parses number key as number', () => {
+test('Parses number key as number', async () => {
     const parser = new Parser('{"test": 1}', "test");
 
-    expect(parser.dump().replace(tabNewline, '')).toBe("interface test {test: number;}");
+    expect((await parser.dump()).replace(tabNewline, '')).toBe("export interface test {test: number;}");
 });
 
-test('Parses boolean key as boolean', () => {
+test('Parses boolean key as boolean', async () => {
     const parser = new Parser('{"test": false, "test2": true}', "test");
 
-    expect(parser.dump().replace(tabNewline, '')).toBe("interface test {test: boolean;test2: boolean;}");
+    expect((await parser.dump()).replace(tabNewline, '')).toBe("export interface test {test: boolean;test2: boolean;}");
 });
 
-test('Creates nested interfaces', () => {
+test('Creates nested interfaces', async () => {
     const parser = new Parser('{"obj": {"p": 1}}', "test");
 
-    expect(parser.dump().replace(tabNewline, '')).toBe("interface test {obj: obj;}interface obj {p: number;}");
+    expect((await parser.dump()).replace(tabNewline, '')).toBe("export interface test {obj: obj;}export interface obj {p: number;}");
 });
 
 /**
  * Arrays
  */
 
-test('Parses string array as string array', () => {
+test('Parses string array as string array', async () => {
     const parser = new Parser('{"test": ["a", "b", "c"]}', "test");
 
-    expect(parser.dump().replace(tabNewline, '')).toBe("interface test {test: Array<string>;}");
+    expect((await parser.dump()).replace(tabNewline, '')).toBe("export interface test {test: Array<string>;}");
 });
 
-test('Parses number array as number array', () => {
+test('Parses number array as number array', async () => {
     const parser = new Parser('{"test": [1,2,3]}', "test");
 
-    expect(parser.dump().replace(tabNewline, '')).toBe("interface test {test: Array<number>;}");
+    expect((await parser.dump()).replace(tabNewline, '')).toBe("export interface test {test: Array<number>;}");
 });
 
-test('Parses boolean array as boolean array', () => {
+test('Parses boolean array as boolean array', async () => {
     const parser = new Parser('{"test": [true, false, true]}', "test");
 
-    expect(parser.dump().replace(tabNewline, '')).toBe("interface test {test: Array<boolean>;}");
+    expect((await parser.dump()).replace(tabNewline, '')).toBe("export interface test {test: Array<boolean>;}");
 });
 
-test('Parses mixed array as any array', () => {
+test('Parses mixed array as any array', async () => {
     const parser = new Parser('{"test": ["a", 1, true]}', "test");
 
-    expect(parser.dump().replace(tabNewline, '')).toBe("interface test {test: Array<any>;}");
+    expect((await parser.dump()).replace(tabNewline, '')).toBe("export interface test {test: Array<any>;}");
 
     parser.load('{"test": ["a", 1]}');
 
-    expect(parser.dump().replace(tabNewline, '')).toBe("interface test {test: Array<any>;}");
+    expect((await parser.dump()).replace(tabNewline, '')).toBe("export interface test {test: Array<any>;}");
 
     parser.load('{"test": ["a", true]}');
 
-    expect(parser.dump().replace(tabNewline, '')).toBe("interface test {test: Array<any>;}");
+    expect((await parser.dump()).replace(tabNewline, '')).toBe("export interface test {test: Array<any>;}");
 
     parser.load('{"test": [1, true]}');
 
-    expect(parser.dump().replace(tabNewline, '')).toBe("interface test {test: Array<any>;}");
+    expect((await parser.dump()).replace(tabNewline, '')).toBe("export interface test {test: Array<any>;}");
 
     parser.load('{"test": [{}, 1, true]}');
 
-    expect(parser.dump().replace(tabNewline, '')).toBe("interface test {test: Array<any>;}");
+    expect((await parser.dump()).replace(tabNewline, '')).toBe("export interface test {test: Array<any>;}");
 });
 
-test('Parses nested arrays', () => {
+test('Parses nested arrays', async () => {
     const parser = new Parser('{"test": [["a"], ["b"]]}', "test");
 
-    expect(parser.dump().replace(tabNewline, '')).toBe("interface test {test: Array<Array<string>>;}");
+    expect((await parser.dump()).replace(tabNewline, '')).toBe("export interface test {test: Array<Array<string>>;}");
 });
 
-test('Parses mixed nested arrays', () => {
+test('Parses mixed nested arrays', async () => {
     const parser = new Parser('{"test": [["a"], [1]]}', "test");
 
-    expect(parser.dump().replace(tabNewline, '')).toBe("interface test {test: Array<Array<any>>;}");
+    expect((await parser.dump()).replace(tabNewline, '')).toBe("export interface test {test: Array<Array<any>>;}");
 
     parser.load('{"test": [["a"], [1], [{}]]}');
 
-    expect(parser.dump().replace(tabNewline, '')).toBe("interface test {test: Array<Array<any>>;}");
+    expect((await parser.dump()).replace(tabNewline, '')).toBe("export interface test {test: Array<Array<any>>;}");
 });
 
-test('Parses mixed array with nested array', () => {
+test('Parses mixed array with nested array', async () => {
     const parser = new Parser('{"test": [["a"], ["b"], "c", 1, 2, ["3"]]}', "test");
 
-    expect(parser.dump().replace(tabNewline, '')).toBe("interface test {test: Array<any>;}");
+    expect((await parser.dump()).replace(tabNewline, '')).toBe("export interface test {test: Array<any>;}");
 });
 
-test('Creates interface for array', () => {
+test('Creates interface for array', async () => {
     const parser = new Parser('{"obj": [{"p": 1}]}', "test");
 
-    expect(parser.dump().replace(tabNewline, '')).toBe("interface test {obj: Array<obj>;}interface obj {p: number;}");
+    expect((await parser.dump()).replace(tabNewline, '')).toBe("export interface test {obj: Array<obj>;}export interface obj {p: number;}");
 });
 
-test('Creates any typed interface key for mixed type object key array', () => {
+test('Creates any typed interface key for mixed type object key array', async () => {
     const parser = new Parser('{"obj": [{"p": 1}, {"p": "a"}]}', "test");
 
-    expect(parser.dump().replace(tabNewline, '')).toBe("interface test {obj: Array<obj>;}interface obj {p: any;}");
+    expect((await parser.dump()).replace(tabNewline, '')).toBe("export interface test {obj: Array<obj>;}export interface obj {p: any;}");
 });
 
-test('Creates any typed interface key for mixed type object array', () => {
+test('Creates any typed interface key for mixed type object array', async () => {
     const parser = new Parser('{"obj": [{"p": 1}, {"p": "a", "m": "x"}]}', "test");
 
-    expect(parser.dump().replace(tabNewline, '')).toBe("interface test {obj: Array<any>;}");
+    expect((await parser.dump()).replace(tabNewline, '')).toBe("export interface test {obj: Array<any>;}");
 });
 
 /**
